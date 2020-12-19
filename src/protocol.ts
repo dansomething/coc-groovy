2282716773af4531fa69c582588064f0423eeabc
Dan Hansen <dan@powerproleasing.com>
2020-12-19 11:55:57 -0600

    Upgrade dependencies and remove unused ones

--- src/protocol.ts
+++ src/protocol.ts
@@ -1,5 +1,5 @@
 import { NotificationType } from 'vscode-languageserver-protocol';
 
 export namespace ProgressReportNotification {
-  export const type = new NotificationType<any, never>('workspace/didChangeConfiguration');
+  export const type = new NotificationType<any>('workspace/didChangeConfiguration');
 }
