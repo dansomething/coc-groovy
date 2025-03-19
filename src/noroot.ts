6c8d227236f09171eb197fc48ec70c23d01cebe5
Dan Hansen <hansendanr+git@gmail.com>
2025-03-19 14:04:49 -0500

    Add NoRootFeature
    When enabled, this feature will will set the Groovy Language Server
    initialization params.rootPath and params.rootUri to `null`. When
    params.rootPath is `null`, the language server will track and compile
    individual .groovy files that don't belong to a workspace root.

---
+++ src/noroot.ts
@@ -0,0 +1,24 @@
+import { StaticFeature, workspace } from 'coc.nvim';
+import { InitializeParams } from 'vscode-languageserver-protocol';
+import { GROOVY } from './constants';
+import { Settings } from './settings';
+
+/**
+ * Feature to support Groovy language server features on `.groovy` files that don't have a workspace root.
+ */
+export class NoRootFeature implements StaticFeature {
+  public fillInitializeParams(params: InitializeParams): void {
+    const config = workspace.getConfiguration(GROOVY);
+    const isNoRoot = config.get<boolean>(Settings.LS_FEATURE_NOROOT, false);
+    if (isNoRoot) {
+      // When the rootPath is null, the Groovy language server will not resolve workspace folders.
+      // Instead, it will track and compile individual opened files.
+      params.rootPath = null;
+      params.rootUri = null;
+    }
+  }
+
+  public fillClientCapabilities(): void {}
+
+  public initialize(): void {}
+}
