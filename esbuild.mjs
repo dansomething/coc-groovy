057fb99fe8aaf7802190ea807f3b41dd2ecc45ec
Dan Hansen <hansendanr+git@gmail.com>
2025-07-02 21:08:01 -0500

    chore(build): migrate from webpack to esbuild
    Replace webpack and related dependencies with esbuild for faster and simpler
    builds. Add `esbuild.mjs` build script and update dependencies in
    `package-lock.json`. Remove unused webpack, ts-loader, and related packages.
    Update engine and dependency versions as needed for esbuild compatibility.

---
+++ esbuild.mjs
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import * as esbuild from 'esbuild';
+
+const loadJSON = (path) => JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
+const data = loadJSON('package.json');
+
+const options = {
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  minify: process.env.NODE_ENV !== 'development',
+  sourcemap: process.env.NODE_ENV === 'development',
+  mainFields: ['module', 'main'],
+  external: ['coc.nvim'],
+  platform: 'node',
+  target: 'node18',
+  outfile: data.main,
+  // https://esbuild.github.io/api/#log-level
+  logLevel: process.env.NODE_ENV === 'development' ? 'info' : 'error',
+};
+
+if (process.argv.length > 2 && process.argv[2] === '--watch') {
+  const ctx = await esbuild.context(options);
+  await ctx.watch();
+  console.log('watching...');
+} else {
+  const result = await esbuild.build(options);
+  if (result.errors.length) {
+    console.error(result.errors);
+  }
+}
